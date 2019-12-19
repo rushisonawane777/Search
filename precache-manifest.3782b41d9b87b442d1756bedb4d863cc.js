86cb8b7d4a07149236e4149d12e3aa717edf61c8
rushisonawane777 <rushisonawane777@gmail.com>
2019-12-19 19:46:40 +0530

    Updates

---
+++ precache-manifest.3782b41d9b87b442d1756bedb4d863cc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b1212c37f06bfbb5a03b5ec6bc0d6f4",
+    "url": "/Search/index.html"
+  },
+  {
+    "revision": "ee91fcb9b0c4e2de6430",
+    "url": "/Search/static/css/main.dc1cb572.chunk.css"
+  },
+  {
+    "revision": "7355c14aaf62b74826b9",
+    "url": "/Search/static/js/2.b02be2e4.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/Search/static/js/2.b02be2e4.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ee91fcb9b0c4e2de6430",
+    "url": "/Search/static/js/main.44f873bd.chunk.js"
+  },
+  {
+    "revision": "2248f307257d2714e6ba",
+    "url": "/Search/static/js/runtime-main.c1f2b24b.js"
+  }
+]);
